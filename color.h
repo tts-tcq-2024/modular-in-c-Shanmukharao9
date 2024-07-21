43146118ea122b9735a4bc33a5ccfec5560778cc
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-07-21 17:48:27 +0530

    Create color.h
    header file

---
+++ color.h
@@ -0,0 +1,22 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void PrintColorCodingReferenceManual();
+
+#endif // COLOR_H
