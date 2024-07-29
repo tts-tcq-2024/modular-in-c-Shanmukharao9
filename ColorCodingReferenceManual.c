37f661c3be5c759a34e4012dbd41b68771a05fbe
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-07-30 01:33:09 +0530

    Create ColorCodingReferenceManual.c

---
+++ ColorCodingReferenceManual.c
@@ -0,0 +1,10 @@
+#include "ColorCodingReferenceManual.h"
+
+void PrintColorCodingReferenceManual() {
+    char colorPairNames[16];
+    for (int pairNumber = 1; pairNumber <= numberOfMajorColors * numberOfMinorColors; pairNumber++) {
+        ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+        ColorPairToString(&colorPair, colorPairNames);
+        printf("%d: %s\n", pairNumber, colorPairNames);
+    }
+}
