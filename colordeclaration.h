0a5548568950c44e43ad2e049ef60e3aadf2bdef
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-04 18:03:25 +0530

    Update colordeclaration.h

--- colordeclaration.h
+++ colordeclaration.h
@@ -1,6 +1,8 @@
 #ifndef COLORDECLARATION_H
 #define COLORDECLARATION_H
+
 #include <stdio.h>
+#include <assert.h>
 #include <stdbool.h>
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
