6814986ba807b1a53703f66bfaf26e56a4c68723
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-04 18:32:46 +0530

    Create PairNumberFromColor.c

---
+++ PairNumberFromColor.c
@@ -0,0 +1,6 @@
+#include "PairNumberFromColor.h"
+#include "colordeclaration.h"
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
+}
