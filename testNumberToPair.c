941d83e4d81ac5cb282fbef335f6b33c06dd733d
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-04 18:41:01 +0530

    Create testNumberToPair.c

---
+++ testNumberToPair.c
@@ -0,0 +1,13 @@
+#include "convertColorpairString.h"
+#include "identifyColorParnumber.h"
+#include "testNumberToPair.h"
+#include "colordeclaration.h"
+
+void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
