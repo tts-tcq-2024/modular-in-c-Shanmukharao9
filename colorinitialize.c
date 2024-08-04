4354937454a4215cfb9a4824e73cad18921380aa
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-04 19:08:22 +0530

    Update colorinitialize.c

--- colorinitialize.c
+++ colorinitialize.c
@@ -1,3 +1,4 @@
+
 #include "colordeclaration.h"
 
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
@@ -7,4 +8,3 @@ int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
